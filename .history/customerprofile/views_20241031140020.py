be44e2fed0f9cdff903bc25ec09f7028f83cc5a1
Mohammad Roudbari <mohammadroodbari2019@gmail.com>
2024-10-31 14:27:59 +0330

    Views and Serializers and Urls are added.

--- .history/customerprofile/views_20241031140020.py
+++ .history/customerprofile/views_20241031140020.py
@@ -1,14 +1,12 @@
+# views.py
 from rest_framework import generics
 from .models import CustomerProfile
-from .serializers import *
-from rest_framework.permissions import *
+from .serializers import CustomerProfileSerializer
 
 class CustomerProfileListCreateView(generics.ListCreateAPIView):
     queryset = CustomerProfile.objects.all()
     serializer_class = CustomerProfileSerializer
-    permission_classes = [IsAuthenticated]
 
 class CustomerProfileRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     queryset = CustomerProfile.objects.all()
     serializer_class = CustomerProfileSerializer
-    permission_classes = [IsAuthenticated]
--- .history/customerprofile/views_20241031142537.py
+++ .history/customerprofile/views_20241031142537.py
@@ -6,9 +6,7 @@ from rest_framework.permissions import *
 class CustomerProfileListCreateView(generics.ListCreateAPIView):
     queryset = CustomerProfile.objects.all()
     serializer_class = CustomerProfileSerializer
-    permission_classes = [IsAuthenticated]
 
 class CustomerProfileRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     queryset = CustomerProfile.objects.all()
     serializer_class = CustomerProfileSerializer
-    permission_classes = [IsAuthenticated]
