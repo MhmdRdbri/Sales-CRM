02ce3e2d8a6897bd3bda58b7c612075adbad8a37
mohammadali <mr.mohammadalisalehi@gmail.com>
2024-11-12 14:40:22 +0330

    debug the serializer

--- marketing/serializers.py
+++ marketing/serializers.py
@@ -4,5 +4,5 @@ from .models import *
 class MarketingSerializer(serializers.ModelSerializer):
     
     class Meta:
-        mdoel = Marketing
+        model = Marketing
         fields = ["id","campaign_name","start_date","end_date","message","target_audiences"]
