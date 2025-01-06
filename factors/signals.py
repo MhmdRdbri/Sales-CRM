c4595e3aaae9bd730dc5d671ba50cb4e1b001b75
mohammadali <mr.mohammadalisalehi@gmail.com>
2025-01-06 17:22:42 +0330

    buer rank atoumations bug fixed

--- factors/signals.py
+++ factors/signals.py
@@ -1,10 +1,16 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Factors
-
 from .tasks import update_customer_rank_task
 
+# Trigger rank update when a factor is created or updated
 @receiver(post_save, sender=Factors)
 def trigger_customer_rank_update(sender, instance, **kwargs):
+    if instance.costumer:
+        update_customer_rank_task.delay(instance.costumer.id)
+
+# Trigger rank update when a factor is deleted
+@receiver(post_delete, sender=Factors)
+def trigger_customer_rank_update_on_delete(sender, instance, **kwargs):
     if instance.costumer:
         update_customer_rank_task.delay(instance.costumer.id)
--- factors/tasks.py
+++ factors/tasks.py
@@ -4,19 +4,26 @@ from .models import CustomerProfile, Factors
 
 @shared_task
 def update_customer_rank_task(customer_id):
-    customer = CustomerProfile.objects.get(id=customer_id)
-    global_avg = Factors.objects.filter(costumer__factor__isnull=False) \
-        .aggregate(avg_price=Avg('price'))['avg_price'] or 0
+    try:
+        customer = CustomerProfile.objects.get(id=customer_id)
 
-    customer_spent = customer.factor.aggregate(total_spent=Sum('price'))['total_spent'] or 0
+        # Calculate the global average price
+        global_avg = Factors.objects.aggregate(avg_price=Avg('price'))['avg_price'] or 0
 
-    if customer_spent > global_avg:
-        new_rank = CustomerProfile.GOLD
-    elif customer_spent < global_avg:
-        new_rank = CustomerProfile.SILVER
-    else:
-        new_rank = CustomerProfile.BRONZE
+        # Calculate the total amount spent by this customer
+        customer_spent = customer.factor.aggregate(total_spent=Sum('price'))['total_spent'] or 0
 
-    if customer.buyer_rank != new_rank:
-        customer.buyer_rank = new_rank
-        customer.save()
+        # Determine new rank based on total spent compared to global average
+        if customer_spent > global_avg:
+            new_rank = CustomerProfile.GOLD
+        elif customer_spent < global_avg:
+            new_rank = CustomerProfile.SILVER
+        else:
+            new_rank = CustomerProfile.BRONZE
+
+        # Update rank only if it has changed
+        if customer.buyer_rank != new_rank:
+            customer.buyer_rank = new_rank
+            customer.save()
+    except CustomerProfile.DoesNotExist:
+        pass  # Handle cases where the customer profile no longer exists
